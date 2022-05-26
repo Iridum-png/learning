9c5ec43596216bf74a881bbd92813b926c6b403c
Iridum-png <baker-e16@boswells-school.com>
2022-05-26 08:24:00 +0100

    Function 3 made

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate rand;
 use rand::Rng;
+use std::collections::HashMap;
 
 fn main() {
+    // Call function 2
     let deck = (1..53).collect::<Vec<u32>>();
     _two(deck);
+
+    // Call function 3
 }
 
 fn _one(num1: i16, num2: i16, num3: i16) -> i16 {
@@ -19,6 +23,14 @@ fn _two(deck: Vec<u32>) {
 
     let choice = deck[random.gen_range(0..52)];
     println!("{}", choice)
+}
+
+fn three<'a>(key_value: HashMap<&'a str, &str>) -> Vec<&'a str> {
+    let mut result: Vec<&'a str> = Vec::new();
+    for pair in key_value.iter() {
+        result.push(pair.0);
+    }
+    return result;
 }
 
 // def three(array):
